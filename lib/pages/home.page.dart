9b2af753dfc65eeb0a30ef745b7170ad078cbd2f
Santa160 <Santa@globizs.com>
2022-09-03 16:25:41 +0530

    TODO UPLOAD IMAGE TO FIRESTORAGE

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,6 +1,9 @@
-import 'package:auto_route/auto_route.dart';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:qrscanner/router/router.gr.dart';
 import 'package:qrscanner/widgets/login_icon.dart';
 
 class HomePage extends StatefulWidget {
@@ -46,187 +49,188 @@ class _HomePageState extends State<HomePage> {
               )
         ],
       ),
-      body: Container(
-        color: Colors.purple[50],
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  context.router.push(const QrCreateRoute());
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.qr_code,
-                      size: MediaQuery.of(context).size.width / 3,
-                    ),
-                    Text(
-                      "Create custom QR",
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * .048,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent),
-                    )
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  context.router.push(const QrScannerRoute());
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.qr_code_scanner,
-                      size: MediaQuery.of(context).size.width / 3,
-                    ),
-                    Text(
-                      "QR Code Scanner",
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * .048,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent),
-                    )
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  context.router.push(const AadhaarScannerRoute());
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.qr_code_2,
-                      size: MediaQuery.of(context).size.width / 3,
-                    ),
-                    Text(
-                      "Aadhaar Scanner",
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * .048,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent),
-                    )
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  showModalBottomSheet(
-                      isScrollControlled: true,
-                      context: context,
-                      builder: ((context) {
-                        return Container(
-                          height: MediaQuery.of(context).size.height * .3,
-                          padding: const EdgeInsets.all(20),
-                          color: Colors.purple[50],
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                children: [
-                                  const Text(
-                                    "Select an Option ",
-                                    style: TextStyle(fontSize: 30),
-                                  ),
-                                  SizedBox(
-                                    height:
-                                        MediaQuery.of(context).size.height / 25,
-                                  ),
-                                  Stack(
-                                    alignment: Alignment.bottomRight,
-                                    clipBehavior: Clip.none,
-                                    children: const [
-                                      Opacity(
-                                        opacity: .9,
-                                        child: Positioned(
-                                          bottom: -59,
-                                          child: Icon(
-                                            Icons.photo_camera_back_outlined,
-                                            size: 80,
-                                            color: Colors.blueGrey,
-                                          ),
-                                        ),
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  buildButton(
-                                      icon: Icons.image_outlined,
-                                      title: 'Open Gallery',
-                                      onClicked: () {
-                                        context.router
-                                            .push(const UploadImageRoute());
-                                      }),
-                                  buildButton(
-                                      icon: Icons.camera_alt_outlined,
-                                      title: 'Open Camera',
-                                      onClicked: () {
-                                        context.router
-                                            .push(const UploadCameraRoute());
-                                      }),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 30,
-                              ),
-                            ],
-                          ),
-                        );
-                      }));
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.upload_outlined,
-                      size: MediaQuery.of(context).size.width / 3,
-                    ),
-                    Text(
-                      "Upload Aadhaar ",
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * .048,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent),
-                    )
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  context.router.push(const CreateIdFolderRoute());
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.upload_file,
-                      size: MediaQuery.of(context).size.width / 3,
-                    ),
-                    Text(
-                      "Upload more IDs",
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * .048,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: const MyWidget(),
+      // body: Container(
+      //   color: Colors.purple[50],
+      //   child: Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //       children: [
+      //         GestureDetector(
+      //           onTap: () {
+      //             context.router.push(const QrCreateRoute());
+      //           },
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: [
+      //               Icon(
+      //                 Icons.qr_code,
+      //                 size: MediaQuery.of(context).size.width / 3,
+      //               ),
+      //               Text(
+      //                 "Create custom QR",
+      //                 style: TextStyle(
+      //                     fontSize: MediaQuery.of(context).size.width * .048,
+      //                     fontWeight: FontWeight.bold,
+      //                     color: Colors.blueAccent),
+      //               )
+      //             ],
+      //           ),
+      //         ),
+      //         GestureDetector(
+      //           onTap: () {
+      //             context.router.push(const QrScannerRoute());
+      //           },
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: [
+      //               Icon(
+      //                 Icons.qr_code_scanner,
+      //                 size: MediaQuery.of(context).size.width / 3,
+      //               ),
+      //               Text(
+      //                 "QR Code Scanner",
+      //                 style: TextStyle(
+      //                     fontSize: MediaQuery.of(context).size.width * .048,
+      //                     fontWeight: FontWeight.bold,
+      //                     color: Colors.blueAccent),
+      //               )
+      //             ],
+      //           ),
+      //         ),
+      //         GestureDetector(
+      //           onTap: () {
+      //             context.router.push(const AadhaarScannerRoute());
+      //           },
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: [
+      //               Icon(
+      //                 Icons.qr_code_2,
+      //                 size: MediaQuery.of(context).size.width / 3,
+      //               ),
+      //               Text(
+      //                 "Aadhaar Scanner",
+      //                 style: TextStyle(
+      //                     fontSize: MediaQuery.of(context).size.width * .048,
+      //                     fontWeight: FontWeight.bold,
+      //                     color: Colors.blueAccent),
+      //               )
+      //             ],
+      //           ),
+      //         ),
+      //         GestureDetector(
+      //           onTap: () {
+      //             showModalBottomSheet(
+      //                 isScrollControlled: true,
+      //                 context: context,
+      //                 builder: ((context) {
+      //                   return Container(
+      //                     height: MediaQuery.of(context).size.height * .3,
+      //                     padding: const EdgeInsets.all(20),
+      //                     color: Colors.purple[50],
+      //                     child: Column(
+      //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //                       children: [
+      //                         Column(
+      //                           children: [
+      //                             const Text(
+      //                               "Select an Option ",
+      //                               style: TextStyle(fontSize: 30),
+      //                             ),
+      //                             SizedBox(
+      //                               height:
+      //                                   MediaQuery.of(context).size.height / 25,
+      //                             ),
+      //                             Stack(
+      //                               alignment: Alignment.bottomRight,
+      //                               clipBehavior: Clip.none,
+      //                               children: const [
+      //                                 Opacity(
+      //                                   opacity: .9,
+      //                                   child: Positioned(
+      //                                     bottom: -59,
+      //                                     child: Icon(
+      //                                       Icons.photo_camera_back_outlined,
+      //                                       size: 80,
+      //                                       color: Colors.blueGrey,
+      //                                     ),
+      //                                   ),
+      //                                 )
+      //                               ],
+      //                             )
+      //                           ],
+      //                         ),
+      //                         Row(
+      //                           mainAxisAlignment:
+      //                               MainAxisAlignment.spaceEvenly,
+      //                           children: [
+      //                             buildButton(
+      //                                 icon: Icons.image_outlined,
+      //                                 title: 'Open Gallery',
+      //                                 onClicked: () {
+      //                                   context.router
+      //                                       .push(const UploadImageRoute());
+      //                                 }),
+      //                             buildButton(
+      //                                 icon: Icons.camera_alt_outlined,
+      //                                 title: 'Open Camera',
+      //                                 onClicked: () {
+      //                                   context.router
+      //                                       .push(const UploadCameraRoute());
+      //                                 }),
+      //                           ],
+      //                         ),
+      //                         const SizedBox(
+      //                           height: 30,
+      //                         ),
+      //                       ],
+      //                     ),
+      //                   );
+      //                 }));
+      //           },
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: [
+      //               Icon(
+      //                 Icons.upload_outlined,
+      //                 size: MediaQuery.of(context).size.width / 3,
+      //               ),
+      //               Text(
+      //                 "Upload Aadhaar ",
+      //                 style: TextStyle(
+      //                     fontSize: MediaQuery.of(context).size.width * .048,
+      //                     fontWeight: FontWeight.bold,
+      //                     color: Colors.blueAccent),
+      //               )
+      //             ],
+      //           ),
+      //         ),
+      //         GestureDetector(
+      //           onTap: () {
+      //             context.router.push(const CreateIdFolderRoute());
+      //           },
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: [
+      //               Icon(
+      //                 Icons.upload_file,
+      //                 size: MediaQuery.of(context).size.width / 3,
+      //               ),
+      //               Text(
+      //                 "Upload more IDs",
+      //                 style: TextStyle(
+      //                     fontSize: MediaQuery.of(context).size.width * .048,
+      //                     fontWeight: FontWeight.bold,
+      //                     color: Colors.blueAccent),
+      //               )
+      //             ],
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 
@@ -249,5 +253,50 @@ class _HomePageState extends State<HomePage> {
             Text(title)
           ],
         ));
+  }
+}
+
+//TODO UPLOAD IMAGE TO FIRESTORAGE
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  final storage = FirebaseStorage.instance;
+                  final res = await FilePicker.platform.pickFiles(
+                      allowMultiple: false,
+                      type: FileType.custom,
+                      allowedExtensions: ['png', 'jpg']);
+
+                  if (res == null) return;
+
+                  final path = res.files.single.path!;
+
+                  final fileName = res.files.single.name;
+                  final file = File(path);
+                  await storage
+                      .ref("image/$fileName")
+                      .putFile(file)
+                      .whenComplete(() => log("Uploaded"));
+                },
+                child: const Text("Check")),
+          ],
+        )
+      ],
+    );
   }
 }
